453ecccbd6030c7e9d65c574972bc2d748d0a81b
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-23 17:52:33 +0500

    Navigation Setup

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { Navigation } from './navigation/Navigation';
 
-function App() {
-  return (
-    <View style={styles.container}>
-      <Text>APP</Text>
-    </View>
-  );
-}
+const App = () => {
+  return <Navigation />;
+};
 
 export default App;
