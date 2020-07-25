431855200c2cf092b18cba3eccf9e6a8468c2575
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-25 14:10:39 +0500

    Updated navigation for CurrencyList, Themes

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,25 +1,65 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../App/screens/Home';
 import { Options } from '../App/screens/Options';
+import { CurrencyList } from '../App/screens/CurrencyList';
+import { Themes } from '../App/screens/Themes';
 
-const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+export const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <MainStack.Screen name="Options" component={Options} />
+      <MainStack.Screen name="Themes" component={Themes} />
+    </MainStack.Navigator>
+  );
+};
+
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => {
+  return (
+    <ModalStack.Navigator mode="modal">
+      <ModalStack.Screen
+        name="Main"
+        component={MainStackScreen}
+        options={{ headerShown: false }}
+      />
+      <ModalStack.Screen
+        name="CurrencyList"
+        component={CurrencyList}
+        options={({ navigation, route }) => ({
+          title: route.params && route.params.title,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.pop()}
+              style={{ paddingHorizontal: 10 }}
+            >
+              <Entypo name="cross" size={30} color="#4f6d7a" />
+            </TouchableOpacity>
+          ),
+          headerLeft: null,
+        })}
+      />
+    </ModalStack.Navigator>
+  );
+};
 
 export const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="Options" component={Options} />
-      </Stack.Navigator>
+      <ModalStackScreen />
     </NavigationContainer>
   );
 };
