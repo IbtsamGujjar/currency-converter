7e4b18e2c3d84b843a9e344eee38d734ca506757
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-25 14:09:47 +0500

    Updated HomeScreen -> PropType, KeyboardSpacer, Options Nav, Reverse Currencies CTA

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,10 +1,24 @@
 /* eslint-disable react/style-prop-object */
-import React from 'react';
-import { View, StyleSheet, Image, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  Image,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
+import moment from 'moment';
 
 import { Input } from '../components/Input/Input';
 import { Colors } from '../../constants/colors';
+import { Button } from '../components/Button/Button';
+import { KeyboardSpacer } from '../components/KeyboardSpacer/KeyboardSpacer';
 
 const background = require('../../assets/images/background.png');
 const logo = require('../../assets/images/logo.png');
@@ -31,36 +45,115 @@ const styles = StyleSheet.create({
     width: screen.width * 0.25,
     height: screen.width * 0.25,
   },
+  headerText: {
+    color: Colors.white,
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  resultText: {
+    fontSize: 14,
+    color: Colors.white,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  content: {
+    paddingTop: screen.height * 0.1,
+  },
+  header: {
+    alignItems: 'flex-end',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
 });
 
-const Home = () => {
+const Home = ({ navigation }) => {
+  const baseCurrency = 'USD';
+  const quoteCurrency = 'GBP';
+  const conversionRate = 0.89824;
+  const date = '2020-04-13';
+
+  const [value, setValue] = useState('100');
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
   return (
-    <View style={styles.screen}>
-      <StatusBar translucent style="light" backgroundColor={Colors.grey} />
-      <View style={styles.logoContainer}>
-        <Image
-          source={background}
-          style={styles.logoBackground}
-          resizeMode="contain"
-        />
-        <Image source={logo} style={styles.logo} resizeMode="contain" />
-      </View>
-      <Input
-        buttonText="USD"
-        value="123"
-        onButtonPress={() => {}}
-        keyboardType="number-pad"
-        onChangeText={() => {}}
-      />
-      <Input
-        buttonText="GBP"
-        value="123"
-        onButtonPress={() => {}}
-        keyboardType="number-pad"
-        onChangeText={() => {}}
-      />
-    </View>
+    <ScrollView
+      style={[styles.container, { backgroundColor: Colors.grey }]}
+      scrollEnabled={scrollEnabled}
+    >
+      <StatusBar style="light-content" backgroundColor={Colors.grey} />
+      <SafeAreaView scrollEnabled={scrollEnabled}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.push('Options')}>
+            <Entypo name="cog" size={32} color={Colors.white} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={background}
+              style={styles.logoBackground}
+              resizeMode="contain"
+            />
+            <Image source={logo} style={styles.logo} resizeMode="contain" />
+          </View>
+          <Text style={styles.headerText}>Currency Converter</Text>
+          <View style={styles.inputContainer}>
+            <Input
+              buttonText={baseCurrency}
+              value={value}
+              onPress={() => {
+                navigation.push('CurrencyList', {
+                  title: 'Base Currency',
+                  isBase: true,
+                });
+              }}
+              keyboardType="number-pad"
+              onChangeText={(text) => {
+                if (text.length === 0) {
+                  setValue();
+                } else {
+                  setValue(text);
+                }
+              }}
+            />
+            <Input
+              buttonText={quoteCurrency}
+              value={
+                value && `${(parseFloat(value) * conversionRate).toFixed(2)}`
+              }
+              editable={false}
+              onPress={() => {
+                navigation.push('CurrencyList', {
+                  title: 'Quote Currency',
+                  isBase: false,
+                });
+              }}
+            />
+          </View>
+          <Text style={styles.resultText}>
+            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${moment(
+              date,
+              'YYYY-MM-DD'
+            ).format('MMM D, YYYY')}`}
+          </Text>
+          <Button text="Reverse Currencies" onPress={() => {}} />
+        </View>
+      </SafeAreaView>
+      <KeyboardSpacer onToggle={(visible) => setScrollEnabled(visible)} />
+    </ScrollView>
   );
+};
+
+Home.defaultProps = {
+  navigation: {},
+};
+
+Home.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.any),
 };
 
 export { Home };
