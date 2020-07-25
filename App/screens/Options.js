7278d68da0b2433c003084a7092990f49bee5dcb
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-25 14:05:51 +0500

    Updated Options screen:

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,19 +1,25 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { SafeAreaView } from 'react-native';
 import { Entypo, Ionicons, AntDesign } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
 
 import { Divider } from '../components/Divider/Divider';
 import { Row } from '../components/Row/Row';
 
 import { APP_CONSTANTS } from '../../constants/constants';
+import { Colors } from '../../constants/colors';
 import { openExternalLink } from '../../utils/helpers';
 
-export const Options = () => {
+export const Options = ({ navigation }) => {
   return (
     <SafeAreaView>
+      <StatusBar style="dark" backgroundColor={Colors.white} />
       <Row
         title="Themes"
         icon={<Entypo name="chevron-right" size={20} color="black" />}
+        onPress={() => navigation.push('Themes')}
       />
       <Divider />
       <Row
@@ -29,4 +35,12 @@ export const Options = () => {
       <Divider />
     </SafeAreaView>
   );
+};
+
+Options.defaultProps = {
+  navigation: {},
+};
+
+Options.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.any),
 };
