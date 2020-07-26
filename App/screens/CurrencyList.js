c1566bbe45e8c01638464b2a8cde3acaa985983a
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 01:47:09 +0500

    Updated CurrencyList Screen: integrated Redux

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { StatusBar, FlatList, View, StyleSheet } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import { useSafeArea } from 'react-native-safe-area-context';
 
-import currencies from '../../data/currencies.json';
+import currenciesList from '../../data/currencies.json';
 import { Row } from '../components/Row/Row';
 import { Divider } from '../components/Divider/Divider';
 import { Colors } from '../../constants/colors';
+
+import {
+  changeBaseCurrency,
+  changeQuoteCurrency,
+} from '../../store/actions/currencies';
 
 const styles = StyleSheet.create({
   screen: {
@@ -23,32 +29,50 @@ const styles = StyleSheet.create({
   },
 });
 
-const CurrencyList = ({ navigation, route }) => {
+const CurrencyListScreen = ({
+  navigation,
+  route,
+  currencies,
+  changeBaseCurrency,
+  changeQuoteCurrency,
+  themeColor,
+}) => {
   const insets = useSafeArea();
+
+  const { baseCurrency, quoteCurrency } = currencies;
 
   const params = route.params || {};
   const { isBase = true } = params;
-  console.log(isBase, 'BASE');
 
   return (
     <View style={{ ...styles.screen, paddingBottom: insets.bottom }}>
       <StatusBar barStyle="dark-content" backgroundColor={Colors.white} />
       <FlatList
-        data={currencies}
+        data={currenciesList}
         renderItem={({ item }) => {
-          const selected = true;
+          let selected = false;
+          if (isBase && item === baseCurrency) {
+            selected = true;
+          } else if (!isBase && item === quoteCurrency) {
+            selected = true;
+          }
           return (
             <Row
               title={item}
               onPress={() => {
+                if (isBase) {
+                  changeBaseCurrency(item);
+                } else {
+                  changeQuoteCurrency(item);
+                }
                 navigation.pop();
               }}
               icon={
-                selected && (
-                  <View style={[styles.icon, { backgroundColor: Colors.grey }]}>
+                selected ? (
+                  <View style={[styles.icon, { backgroundColor: themeColor }]}>
                     <Entypo name="check" size={20} color={Colors.white} />
                   </View>
-                )
+                ) : null
               }
             />
           );
@@ -60,14 +84,37 @@ const CurrencyList = ({ navigation, route }) => {
   );
 };
 
-CurrencyList.defaultProps = {
+CurrencyListScreen.defaultProps = {
+  themeColor: Colors.grey,
+  currencies: {},
   navigation: {},
   route: {},
+  changeBaseCurrency: () => {},
+  changeQuoteCurrency: () => {},
 };
 
-CurrencyList.propTypes = {
+CurrencyListScreen.propTypes = {
+  themeColor: PropTypes.string,
+  currencies: PropTypes.objectOf(PropTypes.any),
   navigation: PropTypes.objectOf(PropTypes.any),
   route: PropTypes.objectOf(PropTypes.any),
+  changeBaseCurrency: PropTypes.func,
+  changeQuoteCurrency: PropTypes.func,
 };
+
+const mapStateToProps = (state) => ({
+  currencies: state.currencies,
+  themeColor: state.theme.themeColor,
+});
+
+const mapDispatchToProps = {
+  changeBaseCurrency,
+  changeQuoteCurrency,
+};
+
+const CurrencyList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CurrencyListScreen);
 
 export { CurrencyList };
