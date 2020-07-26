19b4ab39009dda9094d87838edae62b1ad231610
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 01:51:04 +0500

    Updated Root App component: Redux, Custom Font load

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 
-import { Navigation } from './navigation/Navigation';
+import { AppNavigator } from './navigation/AppNavigator';
+
+import { store } from './store/store';
+
+const openSansFont = require('./assets/fonts/OpenSans-Regular.ttf');
+const openSansBoldFont = require('./assets/fonts/OpenSans-Bold.ttf');
 
 const App = () => {
-  return <Navigation />;
+  const [fontsLoaded] = useFonts({
+    'open-sans': openSansFont,
+    'open-sans-bold': openSansBoldFont,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
 
 export default App;
