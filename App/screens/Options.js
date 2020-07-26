ee15480b7811ae87c3f1e0def97c8f0681d30fe3
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 01:46:10 +0500

    Updated OptionScreen: integrated Redux

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/style-prop-object */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { SafeAreaView } from 'react-native';
 import { Entypo, Ionicons, AntDesign } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
@@ -12,35 +13,54 @@ import { APP_CONSTANTS } from '../../constants/constants';
 import { Colors } from '../../constants/colors';
 import { openExternalLink } from '../../utils/helpers';
 
-export const Options = ({ navigation }) => {
+import { logout } from '../../store/actions/auth';
+
+export const OptionsScreen = ({ navigation, themeColor, logout }) => {
   return (
     <SafeAreaView>
       <StatusBar style="dark" backgroundColor={Colors.white} />
       <Row
         title="Themes"
-        icon={<Entypo name="chevron-right" size={20} color="black" />}
+        icon={<Entypo name="chevron-right" size={20} color={themeColor} />}
         onPress={() => navigation.push('Themes')}
       />
       <Divider />
       <Row
         title="Fixer.io"
-        icon={<Ionicons name="ios-link" size={20} color="black" />}
+        icon={<Ionicons name="ios-link" size={20} color={themeColor} />}
         onPress={() => openExternalLink(APP_CONSTANTS.FIXER)}
       />
       <Divider />
       <Row
         title="Logout"
-        icon={<AntDesign name="logout" size={20} color="black" />}
+        icon={<AntDesign name="logout" size={20} color={themeColor} />}
+        onPress={logout}
       />
       <Divider />
     </SafeAreaView>
   );
 };
 
-Options.defaultProps = {
+OptionsScreen.defaultProps = {
+  themeColor: Colors.grey,
   navigation: {},
+  logout: () => {},
 };
 
-Options.propTypes = {
+OptionsScreen.propTypes = {
+  themeColor: PropTypes.string,
   navigation: PropTypes.objectOf(PropTypes.any),
+  logout: PropTypes.func,
 };
+
+const mapStateToProps = (state) => ({
+  themeColor: state.theme.themeColor,
+});
+
+const mapDispatchToProps = {
+  logout,
+};
+
+const Options = connect(mapStateToProps, mapDispatchToProps)(OptionsScreen);
+
+export { Options };
