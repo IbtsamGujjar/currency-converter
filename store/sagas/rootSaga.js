45922df304adf9595845a2d8f4c04f1193f51067
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 01:39:10 +0500

    Updated rootSaga

--- store/sagas/rootSaga.js
+++ store/sagas/rootSaga.js
@@ -1,3 +1,23 @@
+import { takeLatest } from 'redux-saga/effects';
+
+import {
+  GET_INITIAL_CONVERSION_START,
+  SWAP_CURRENCY_START,
+  CHANGE_BASE_CURRENCY_START,
+  LOGIN_START,
+} from '../../constants/actionTypes';
+
+import {
+  getInitialConversions,
+  swapCurrencies,
+  changeBaseCurrency,
+} from './currenciesSaga';
+
+import { login } from './authSaga';
+
 export function* rootSaga() {
-  yield;
+  yield takeLatest(LOGIN_START, login);
+  yield takeLatest(GET_INITIAL_CONVERSION_START, getInitialConversions);
+  yield takeLatest(SWAP_CURRENCY_START, swapCurrencies);
+  yield takeLatest(CHANGE_BASE_CURRENCY_START, changeBaseCurrency);
 }
