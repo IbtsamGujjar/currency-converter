d47a04f70b54a8a58fd04cb9664a41d51b881ee8
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 01:39:40 +0500

    Updated combineReducer

--- store/reducers/combineReducer.js
+++ store/reducers/combineReducer.js
@@ -1,3 +1,10 @@
 import { combineReducers } from 'redux';
+import { currenciesReducer } from './currenciesReducer';
+import { themeReducer } from './themeReducer';
+import { authReducer } from './authReducer';
 
-export const combineReducer = combineReducers({});
+export const combineReducer = combineReducers({
+  currencies: currenciesReducer,
+  theme: themeReducer,
+  user: authReducer,
+});
