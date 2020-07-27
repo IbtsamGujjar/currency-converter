4939e9dd481f0c458f7364f4979ad96c2662b3ec
Ibtsam Noman <ibtsam.noman@Cdocs.local>
2020-07-27 17:51:22 +0500

    storybook setup

---
+++ storybook/storyLoader.js
@@ -0,0 +1,25 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../App/components/Button/Button.stories');
+  require('../App/components/Divider/Divider.stories');
+  require('../App/components/FormInput/FormInput.stories');
+  require('../App/components/Input/Input.stories');
+  require('../App/components/Row/Row.stories');
+}
+
+const stories = [
+  '../App/components/Button/Button.stories',
+  '../App/components/Divider/Divider.stories',
+  '../App/components/FormInput/FormInput.stories',
+  '../App/components/Input/Input.stories',
+  '../App/components/Row/Row.stories',
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
